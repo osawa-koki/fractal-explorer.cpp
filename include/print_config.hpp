1b1194fa98f4a30448a85b88d5b35c86ef118d13
osawa-koki <github@osawa.dev>
2023-03-27 14:49:05 +0900

    print_config

---
+++ include/print_config.hpp
@@ -0,0 +1,26 @@
+#ifndef print_config_hpp
+#define print_config_hpp
+
+#include <iostream>
+#include <string>
+
+#include "config.hpp"
+
+void print_config(const Config& config) {
+  std::cout << "width: " << config.width << std::endl;
+  std::cout << "height: " << config.height << std::endl;
+  std::cout << "output_directory: " << config.output_directory << std::endl;
+
+  std::cout << "===== ===== ===== Mandelbrot ===== ===== =====" << std::endl;
+  std::cout << "x_min: " << config.mandelbrot.x_min << std::endl;
+  std::cout << "x_max: " << config.mandelbrot.x_max << std::endl;
+  std::cout << "y_min: " << config.mandelbrot.y_min << std::endl;
+  std::cout << "y_max: " << config.mandelbrot.y_max << std::endl;
+  std::cout << "threshold: " << config.mandelbrot.threshold << std::endl;
+  std::cout << "max_iteration: " << config.mandelbrot.max_iterations << std::endl;
+  std::cout << "color_hue: " << config.mandelbrot.color_hue << std::endl;
+  std::cout << "background_color_hex: " << config.mandelbrot.background_color_hex << std::endl;
+  std::cout << "output_file: " << config.mandelbrot.output_file << std::endl;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include "config.hpp"
 #include "commandline_parser.hpp"
+#include "print_config.hpp"
 
 int main() {
 
@@ -11,20 +12,7 @@ int main() {
   Config config = *config_ptr;
   delete config_ptr;
 
-  std::cout << "width: " << config.width << std::endl;
-  std::cout << "height: " << config.height << std::endl;
-  std::cout << "output_directory: " << config.output_directory << std::endl;
-
-  std::cout << "===== ===== ===== Mandelbrot ===== ===== =====" << std::endl;
-  std::cout << "x_min: " << config.mandelbrot.x_min << std::endl;
-  std::cout << "x_max: " << config.mandelbrot.x_max << std::endl;
-  std::cout << "y_min: " << config.mandelbrot.y_min << std::endl;
-  std::cout << "y_max: " << config.mandelbrot.y_max << std::endl;
-  std::cout << "threshold: " << config.mandelbrot.threshold << std::endl;
-  std::cout << "max_iteration: " << config.mandelbrot.max_iterations << std::endl;
-  std::cout << "color_hue: " << config.mandelbrot.color_hue << std::endl;
-  std::cout << "background_color_hex: " << config.mandelbrot.background_color_hex << std::endl;
-  std::cout << "output_file: " << config.mandelbrot.output_file << std::endl;
+  print_config(config);
 
   return 0;
 }
