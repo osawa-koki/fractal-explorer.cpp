e446a0c5712a78da42a7164db56e16ecea541276
osawa-koki <github@osawa.dev>
2023-03-27 19:03:13 +0900

    小さな修正。

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@ target_dir="./dist"
 target_file="$target_dir/items.txt"
 rm -f "$target_file"
 
-make clean
-make
-make run
+# make clean
+# make
+# make run
 
 files=$(find $target_dir -type f -name '*.png')
 for file in $files; do
-  echo "$file" >> "$target_file"
+  file_name=$(basename "$file")
+  if [ "$file_name" == "tako.png" ]; then
+    continue
+  fi
+  echo "$file_name" >> "$target_file"
+  echo "$file_name"
 done
