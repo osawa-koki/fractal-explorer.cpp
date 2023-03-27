28bde944950a40dd3375fbcfcfec83684a1df00c
osawa-koki <github@osawa.dev>
2023-03-27 14:59:25 +0900

    不要なコードを消した。

--- include/commandline_parser.hpp
+++ include/commandline_parser.hpp
@@ -45,20 +45,5 @@ Config* commandline_parser() {
   mandelbrot.output_file = mandelbrot_node.child("output_file").child_value();
   config->mandelbrot = mandelbrot;
 
-  std::cout << "width: " << config->width << std::endl;
-  std::cout << "height: " << config->height << std::endl;
-  std::cout << "output_directory: " << config->output_directory << std::endl;
-
-  std::cout << "===== ===== ===== Mandelbrot ===== ===== =====" << std::endl;
-  std::cout << "x_min: " << config->mandelbrot.x_min << std::endl;
-  std::cout << "x_max: " << config->mandelbrot.x_max << std::endl;
-  std::cout << "y_min: " << config->mandelbrot.y_min << std::endl;
-  std::cout << "y_max: " << config->mandelbrot.y_max << std::endl;
-  std::cout << "threshold: " << config->mandelbrot.threshold << std::endl;
-  std::cout << "max_iteration: " << config->mandelbrot.max_iterations << std::endl;
-  std::cout << "color_hue: " << config->mandelbrot.color_hue << std::endl;
-  std::cout << "background_color_hex: " << config->mandelbrot.background_color_hex << std::endl;
-  std::cout << "output_file: " << config->mandelbrot.output_file << std::endl;
-
   return config;
 }
